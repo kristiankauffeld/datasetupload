344fc594071b420a12f28689114adb032cde950d
Nils Rasmus Armstrong <niraar@ruc.dk>
2020-12-11 10:16:35 +0100

    Removed inception project and added quasar to vue

---
+++ src/quasar.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+
+import 'quasar/dist/quasar.css'
+import lang from 'quasar/lang/en-us.js'
+import '@quasar/extras/material-icons/material-icons.css'
+import { Quasar } from 'quasar'
+
+Vue.use(Quasar, {
+  config: {},
+  plugins: {
+  },
+  lang: lang
+ })
